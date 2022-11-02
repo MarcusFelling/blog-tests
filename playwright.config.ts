8d6ac37efb6b9160e8268f41ad9c465afa739590
Marcus Felling <marcus.felling@gmail.com>
2022-11-02 15:30:37 -0500

    remove webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,13 +64,6 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
-
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
