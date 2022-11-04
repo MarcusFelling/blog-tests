d7a4dd7e30b04052431a81953ccc46b3436b070b
Marcus Felling <marcus.felling@gmail.com>
2022-11-04 15:44:07 -0500

    add test to check all links return 200

--- tests/archives.spec.ts
+++ tests/archives.spec.ts
@@ -8,3 +8,17 @@ test('should allow me to view archives', async ({ page }) => {
   // Assert posts are shown
   await expect(page.locator('.entry-content')).toBeDefined();
 });
+
+test('should allow me to check all links', async ({ page, request }) => {
+  await page.goto('/archives');
+  // Get all links on archives page
+  const links = await page.locator('.entry-content a:visible');
+  const count = await links.count()
+  // Loop through each link and check if response is 200
+  for (let i = 0; i < count; i++) {
+    const link = await links.nth(i);
+    const href = await link.getAttribute('href');
+    const response = await request.get(href);
+    await expect(response.ok()).toBeTruthy();
+  }
+});
